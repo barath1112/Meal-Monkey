d2aea6cb21befc1b52b6967ef623e44b371f2ebe
Barath <99529644+barath1112@users.noreply.github.com>
2025-01-13 13:06:28 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-contents'>
+        <h2>Order Your Favouite Meal</h2>
+        <p className='fonCol'>Welcome to Meal Monkey – your go-to destination for deliciousness! Indulge in a diverse selection of cuisines, crafted to satisfy every craving. Whether it’s a quick bite or a lavish feast, we ensure swift delivery and a seamless ordering experience. Taste happiness, delivered fresh to your door!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
